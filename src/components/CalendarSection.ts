c62703b57fde8831eee981ad275831090f6c947a
Adry <adrydevmateo@gmail.com>
2022-11-26 18:38:58 -0400

    Adding a file to the Utils Folder

--- src/components/CalendarSection.ts
+++ src/components/CalendarSection.ts
@@ -1,3 +1,5 @@
 const calendarSectionElement: HTMLElement = document.createElement("section");
+const ulWeeksElement: HTMLUListElement = document.createElement("ul");
+const ulDaysElement: HTMLUListElement = document.createElement("ul");
 
 export const CalendarSection: HTMLElement = calendarSectionElement;
