bb4878bda6d671a5a3b774fe2d35503209ad37c4
Adry <adrydevmateo@gmail.com>
2022-11-26 19:33:52 -0400

    Stopped working on the logic because of a dependency

--- src/components/Calendar.ts
+++ src/components/Calendar.ts
@@ -1,10 +1,55 @@
 import { currentDate, spanIcons } from "./Header";
 import { daysList } from "./CalendarSection";
+import { months } from "../utils/Collections";
 
 let date = new Date();
 let currentYear = date.getFullYear();
 let currentMonth = date.getMonth();
 
-function renderCalendar() {}
+function renderCalendar() {
+  let firstDayOfMonth: number = new Date(currentYear, currentMonth, 1).getDay();
+  let lastDateOfMonth: number = new Date(
+    currentYear,
+    currentMonth + 1,
+    0
+  ).getDate();
+  let lastDayOfMonth: number = new Date(
+    currentYear,
+    currentMonth,
+    lastDateOfMonth
+  ).getDay();
+  let lastDateOfLastMonth: number = new Date(
+    currentYear,
+    currentMonth,
+    0
+  ).getDate();
+  let newLi: string = "";
+
+  for (let index = firstDayOfMonth; index > 0; index--) {
+    newLi += `<li class="inactive">${lastDateOfLastMonth - index + 1}</li>`;
+  }
+
+  for (let index = 1; index <= lastDateOfMonth; index++) {
+    let today: string =
+      index === date.getDate() &&
+      currentMonth === new Date().getMonth() &&
+      currentYear === new Date().getFullYear()
+        ? "active"
+        : "";
+
+    newLi += `<li class="${today}">${index}</li>`;
+  }
+
+  for (let index = lastDayOfMonth; index < 6; index++) {
+    newLi += `<li class="inactive">${index - lastDayOfMonth + 1}</li>`;
+  }
+
+  currentDate.innerText = `${months[currentMonth]} ${currentYear}`;
+  daysList.innerHTML = newLi;
+}
+
+spanIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {});
+});
 
 export const Calendar: Function = renderCalendar;
