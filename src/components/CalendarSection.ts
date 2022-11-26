c70a6cf69b809ca2f3acdebde733ee8bc344b53e
Adry <adrydevmateo@gmail.com>
2022-11-26 18:58:32 -0400

    Finished working on calendar section layout

--- src/components/CalendarSection.ts
+++ src/components/CalendarSection.ts
@@ -1,5 +1,13 @@
+import { daysAbbreviations } from "../utils/Collections";
+import { CreateListItems } from "../utils/CreateListItems";
+
 const calendarSectionElement: HTMLElement = document.createElement("section");
 const ulWeeksElement: HTMLUListElement = document.createElement("ul");
 const ulDaysElement: HTMLUListElement = document.createElement("ul");
+
+CreateListItems(daysAbbreviations, ulWeeksElement);
+
+calendarSectionElement.appendChild(ulWeeksElement);
+calendarSectionElement.appendChild(ulDaysElement);
 
 export const CalendarSection: HTMLElement = calendarSectionElement;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 import { Header } from "./components/Header";
+import { CalendarSection } from "./components/CalendarSection";
 
 const app = document.querySelector<HTMLDivElement>("#app");
 
 app?.appendChild(Header);
+app?.appendChild(CalendarSection);
