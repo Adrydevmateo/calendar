4790a17927984068e539fa7aba7042829f874be6
Adry <adrydevmateo@gmail.com>
2022-11-26 18:29:52 -0400

    Finished working on the header section layout

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,3 +1,14 @@
+// * Creating HTML Elements
 const headerElement: HTMLHeadElement = document.createElement("header");
+const paragraphElement: HTMLParagraphElement = document.createElement("p");
+const iconsSection: HTMLElement = document.createElement("section");
+const iconsFirstSpan: HTMLSpanElement = document.createElement("span");
+const iconsSecondSpan: HTMLSpanElement = document.createElement("span");
+
+// * Appending Elements
+headerElement.appendChild(paragraphElement);
+headerElement.appendChild(iconsSection);
+iconsSection.appendChild(iconsFirstSpan);
+iconsSection.appendChild(iconsSecondSpan);
 
 export const Header: HTMLHeadElement = headerElement;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import "./style.css";
+import { Header } from "./components/Header";
 
 const app = document.querySelector<HTMLDivElement>("#app");
+
+app?.appendChild(Header);
